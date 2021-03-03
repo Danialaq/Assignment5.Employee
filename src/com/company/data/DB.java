c661ef714dbb1991bd99b38f95b1e504531f9195
Danialaq <79837549+Danialaq@users.noreply.github.com>
2021-03-03 21:37:14 +0600

    Update DB.java

--- src/com/company/data/DB.java
+++ src/com/company/data/DB.java
@@ -6,16 +6,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DB implements IDB{
+public class DB implements IDB{ //подключаем интерфейс 
     @Override
-    public Connection con() throws SQLException, ClassNotFoundException {
-        String url = "jdbc:postgresql://localhost:5432/danial";
+    public Connection con() throws SQLException, ClassNotFoundException { // пользуемся методом из IDB
+        String url = "jdbc:postgresql://localhost:5432/danial"; // пишем какую базу данных будем использовать из сервера
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection y = DriverManager.getConnection(url, "postgres", "0000");
+            Class.forName("org.postgresql.Driver");  
+            Connection y = DriverManager.getConnection(url, "postgres", "0000");  // коннэктим IntelliG с pgAdmin знакомым нам способом, используя DriverManager и его методом getConnection,обязательно прописываем его пакет
             return y;
         } catch (Exception e){
-            System.out.println("No connection!");
+            System.out.println("No connection!"); // я не прописывал какие имена ошибки ловить ему, не видел в этом необходимости
             return null;
         }
     }
