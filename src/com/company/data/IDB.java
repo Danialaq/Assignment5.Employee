b9bc756c516f3b8cfb315552e04ddd778ec0e13f
Danialaq <79837549+Danialaq@users.noreply.github.com>
2021-03-03 21:40:03 +0600

    Update IDB.java

--- src/com/company/data/IDB.java
+++ src/com/company/data/IDB.java
@@ -4,5 +4,5 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface IDB {
-    public Connection con() throws SQLException, ClassNotFoundException;
+    public Connection con() throws SQLException, ClassNotFoundException; // создаем интерфейс и его метод для DB, можно было обойтись без него,но мы показываем что умеем работать с interface ;)  
 }
