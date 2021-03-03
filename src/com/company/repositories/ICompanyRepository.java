30577599caf45c5d8bff6b4bff2e4c8ac933cd1c
Danialaq <79837549+Danialaq@users.noreply.github.com>
2021-03-03 21:43:50 +0600

    Update ICompanyRepository.java

--- src/com/company/repositories/ICompanyRepository.java
+++ src/com/company/repositories/ICompanyRepository.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface ICompanyRepository {
-    public Employee getEmployeeById(int id) throws SQLException, ClassNotFoundException;
-    public boolean addEmployee(Employee employee) throws SQLException, ClassNotFoundException;
-    public boolean removeEmployeeById(int id);
+    public Employee getEmployeeById(int id) throws SQLException, ClassNotFoundException; // создаем методы в интерфейсе для CompanyRepository и CompanyController, to get all info about an employee 
+    public boolean addEmployee(Employee employee) throws SQLException, ClassNotFoundException; //создаем методы в интерфейсе для CompanyRepository и CompanyController, to add a new employee into the database
+    public boolean removeEmployeeById(int id); //создаем методы в интерфейсе для CompanyRepository и CompanyController, to delete a row of an employee from itcomp table
 }
